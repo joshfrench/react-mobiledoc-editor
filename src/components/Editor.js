ca5fdb35fea5a68650aec2e4d832c122d8a1be7d
Josh French <joshfrench@gmail.com>
2016-06-11 09:58:45 -0700

    Add will/did create editor callbacks

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,8 +1,16 @@
 import React from 'react';
 
 const Editor = React.createClass({
+  componentWillMount() {
+    if (typeof this.props.willCreateEditor === 'function') {
+      this.props.willCreateEditor();
+    }
+  },
   componentDidMount() {
-    this.props.editor.render(this.refs.editor);
+    this.props.editor && this.props.editor.render(this.refs.editor);
+    if (typeof this.props.didCreateEditor === 'function') {
+      this.props.didCreateEditor(this.props.editor);
+    }
   },
   render() {
     return <div ref="editor" />;
