b468f9dcc4b4d6c4a17718d0fed838e4703b7420
Josh French <joshfrench@gmail.com>
2016-06-16 16:02:31 -0700

    Add classToCard tests

---
+++ test/utils/classToCardTest.js
@@ -0,0 +1,25 @@
+import {classToDOMCard} from '../../src/utils/classToCard';
+import {expect} from 'chai';
+import {stub} from 'sinon';
+
+describe('classToDOMCard()', () => {
+  const Component = { displayName: "Test" };
+  it('should use explicit name', () => {
+    const card = classToDOMCard(Component, 'Test');
+    expect(card.name).to.eql('Test');
+  });
+
+  it('should infer name from component displayName', () => {
+    const card = classToDOMCard(Component);
+    expect(card.name).to.eql('TestCard');
+  });
+
+  it('returns element created by options.addComponent', () => {
+    const card = classToDOMCard(Component);
+    const destinationElement = "destinationElement";
+    const cardArg = { options: { addComponent: (card) => ({card, destinationElement}) },
+                      env:     { onTeardown: stub() }};
+
+    expect(card.render(cardArg)).to.eql(destinationElement);
+  });
+});
