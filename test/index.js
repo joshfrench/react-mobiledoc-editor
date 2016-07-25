964719474a9143579b0321ae0fa9e95b6008b2fb
Josh French <joshfrench@gmail.com>
2016-07-24 20:43:26 -0700

    WIP: get tests running via phantomjs

---
+++ test/index.js
@@ -0,0 +1,11 @@
+
+// import { describe, it } from 'mocha';
+import { expect } from 'chai';
+
+describe('a test', () => {
+  it('should run', () => {
+    expect(true).to.be.true;
+  });
+});
+
+// phantom.exit();
