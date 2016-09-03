f47b43cee6bbd40a6879f5ce09eb2612c83496f9
Josh French <joshfrench@gmail.com>
2016-09-03 10:48:00 -0700

    Remove drop wrapper from exports

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import AtomControl from './components/AtomControl';
 import CardControl from './components/CardControl';
 import { classToDOMCard } from './utils/classToCard';
 import Container, { EMPTY_MOBILEDOC } from './components/Container';
-import DropWrapper from './components/DropWrapper';
 import Editor from './components/Editor';
 import LinkControl from './components/LinkControl';
 import LinkForm from './components/LinkForm';
@@ -15,7 +14,6 @@ export {
   CardControl,
   classToDOMCard,
   Container,
-  DropWrapper,
   Editor,
   EMPTY_MOBILEDOC,
   LinkControl,
