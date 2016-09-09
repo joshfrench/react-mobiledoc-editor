219c7a617859d17808ea50eb02af757a2d84d70a
Josh French <joshfrench@gmail.com>
2016-09-09 16:13:41 -0700

    Export renamed controls

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import Container, { EMPTY_MOBILEDOC } from './components/Container';
 import Editor from './components/Editor';
 import LinkControl from './components/LinkControl';
 import LinkForm from './components/LinkForm';
-import MarkupControl from './components/MarkupControl';
-import SectionControl from './components/SectionControl';
+import MarkupButton from './components/MarkupButton';
+import SectionButton from './components/SectionButton';
 import Toolbar from './components/Toolbar';
 
 export {
@@ -18,7 +18,7 @@ export {
   EMPTY_MOBILEDOC,
   LinkControl,
   LinkForm,
-  MarkupControl,
-  SectionControl,
+  MarkupButton,
+  SectionButton,
   Toolbar
 };
