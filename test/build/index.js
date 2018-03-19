eb01baff6e4e93d075185904a42b0f3ac3d39868
Garth Poitras <garth22@gmail.com>
2018-03-19 14:49:39 -0400

    Failing test for UMD build

---
+++ test/build/index.js
@@ -0,0 +1,8 @@
+const { ReactMobiledocEditor } = require('../../dist/main');
+const { expect } = require('chai');
+
+describe('build', () => {
+  it('should build UMD compliant output', () => {
+    expect(ReactMobiledocEditor).to.be.ok;
+  });
+});
