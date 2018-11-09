7e2b6df584cae0a580b018d85a8fae8f51a4ecb2
Josh French <josh.french@stitchfix.com>
2018-11-09 08:35:43 -0800

    Editor -> React 16

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,26 +1,24 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-import createReactClass from 'create-react-class';
-
-const Editor = createReactClass({
-  displayName: 'Editor',
-
-  contextTypes: {
-    editor: PropTypes.object
-  },
-
+class Editor extends React.Component {
   componentDidMount() {
     const { editor } = this.context;
     if (editor) {
       editor.render(this.refs.editor);
     }
-  },
+  }
 
   render() {
     const props = { ...this.props, ref: "editor" };
     return <div {...props} />;
   }
-});
+}
+
+Editor.displayName = 'Editor';
+
+Editor.contextTypes = {
+  editor: PropTypes.object
+};
 
 export default Editor;
