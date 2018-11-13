ee6b647bb6f3f8e1b456b40e8aa29626378df12b
Josh French <joshfrench@gmail.com>
2018-11-12 17:53:05 -0800

    Update to latest and greatest deps

--- test/build/index.js
+++ test/build/index.js
@@ -3,6 +3,7 @@ const { expect } = require('chai');
 
 describe('build', () => {
   it('should build UMD compliant output', () => {
-    expect(ReactMobiledocEditor).to.be.an('object');
+    expect(typeof ReactMobiledocEditor).to.equal('object');
+    expect(ReactMobiledocEditor).to.be.a('module');
   });
 });
