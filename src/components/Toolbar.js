fc44f482e82aed01b5145f4baf951fef2f8195d7
Zahra Jabini <zahra.arch@gmail.com>
2020-01-07 09:32:22 -0500

    Removes AttributeButton from default Toolbar 💀
    We don’t need to include AttributeButtons since this functionality is provided by the default AttributeSelect.

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -13,7 +13,6 @@ const Toolbar = (props) =>
     <li><LinkButton /></li>
     <li>Style: <SectionSelect tags={["h1", "h2", "h3"]} /></li>
     <li>Alignment: <AttributeSelect attribute='text-align' values={["left", "center", "right"]} /></li>
-    <li><AttributeButton attribute='text-align' value={"left"} /></li>
     <li><SectionButton tag='blockquote' /></li>
     <li><SectionButton tag='ul'>UL</SectionButton></li>
     <li><SectionButton tag='ol'>OL</SectionButton></li>
